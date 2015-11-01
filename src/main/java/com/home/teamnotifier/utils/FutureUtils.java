8adc4cfa91bf425e6c4f03267c8ead9edaab9628
Goloshchapov Egor <golosegor@gmail.com>
2015-11-01 22:01:44 +0300

    broken build fixed

--- src/main/java/com/home/teamnotifier/utils/FutureUtils.java
+++ src/main/java/com/home/teamnotifier/utils/FutureUtils.java
@@ -1,14 +1,9 @@
 package com.home.teamnotifier.utils;
 
-import com.google.common.collect.Lists;
-
-import java.lang.reflect.Array;
+import static java.util.stream.Collectors.toList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
-import java.util.stream.IntStream;
-
-import static java.util.stream.Collectors.toList;
 
 public final class FutureUtils {
   private FutureUtils() {
@@ -17,31 +12,8 @@ public final class FutureUtils {
 
   public static <V, T extends List<CompletableFuture<V>>> CompletableFuture<List<V>> allAsList(
       final T f,
-      final Executor executor
-  ) {
-    final List<V> values = Lists.newCopyOnWriteArrayList();
-    final List<CompletableFuture<Void>> resultFutures = IntStream.range(0, f.size())
-        .mapToObj(i -> f.get(i).thenAcceptAsync(values::add, executor))
-        .collect(toList());
-
-    final CompletableFuture<Void>[] futuresArray = toGenericArray(resultFutures);
-
-    return CompletableFuture
-        .allOf(futuresArray)
-        .thenApplyAsync(ignored -> values, executor);
-  }
-
-  @SuppressWarnings("unchecked")
-  private static CompletableFuture<Void>[] toGenericArray(
-      final List<CompletableFuture<Void>> futures) {
-    /**
-     * Hack to create generic array in order to use
-     * {@link CompletableFuture#allOf(CompletableFuture[])}
-     * which is use arrays
-     */
-    return (CompletableFuture<Void>[]) Array.newInstance(
-        CompletableFuture.class,
-        futures.size()
-    );
+      final Executor executor) {
+    return CompletableFuture.allOf(f.toArray(new CompletableFuture[f.size()]))
+        .thenApplyAsync(ignored -> f.stream().map(CompletableFuture::join).collect(toList()), executor);
   }
 }
--- src/test/java/com/home/teamnotifier/utils/FutureUtilsTest.java
+++ src/test/java/com/home/teamnotifier/utils/FutureUtilsTest.java
@@ -1,46 +1,45 @@
 package com.home.teamnotifier.utils;
 
 import com.google.common.collect.Lists;
-import com.home.teamnotifier.utils.FutureUtils;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-/**
- * Created by dgoloshc on 17.09.2015.
- */
 public class FutureUtilsTest {
 
-    private final Executor executor = Executors.newCachedThreadPool();
+  private final Executor executor = Executors.newCachedThreadPool();
 
-    private Supplier<Integer> dummyTask(final int returnVal, final int delayMs) {
-        return () -> {
-            try {
-                TimeUnit.MILLISECONDS.sleep(delayMs);
-            } catch (InterruptedException e) {
-                throw new IllegalStateException(e);
-            }
-            return returnVal;
-        };
-    }
+  @Test
+  public void testAllAsList()
+  throws Exception {
+    final CompletableFuture<Integer> f1 = CompletableFuture
+        .supplyAsync(dummyTask(1, 1000), executor);
+    final CompletableFuture<Integer> f2 = CompletableFuture
+        .supplyAsync(dummyTask(2, 2000), executor);
 
-    @Test
-    public void testAllAsList() throws Exception {
-        final CompletableFuture<Integer> f1 = CompletableFuture.supplyAsync(dummyTask(1, 1000), executor);
-        final CompletableFuture<Integer> f2 = CompletableFuture.supplyAsync(dummyTask(2, 2000), executor);
+    List<CompletableFuture<Integer>> futures = Lists.newArrayList(f1, f2);
 
-        @SuppressWarnings("unchecked") ArrayList<CompletableFuture<Integer>> futures = Lists.newArrayList(f1, f2);
+    final List<Integer> integers = FutureUtils.allAsList(futures, executor).join();
 
-        final List<Integer> integers = FutureUtils.allAsList(futures, executor).join();
+    assertThat(integers)
+        .contains(1)
+        .contains(2)
+        .hasSize(futures.size());
+  }
 
-        assertThat(integers)
-                .contains(1)
-                .contains(2)
-                .hasSize(futures.size());
-    }
+  private Supplier<Integer> dummyTask(final int returnVal, final int delayMs) {
+    return () -> {
+      try {
+        TimeUnit.MILLISECONDS.sleep(delayMs);
+      } catch (InterruptedException e) {
+        throw new IllegalStateException(e);
+      }
+      return returnVal;
+    };
+  }
 }
