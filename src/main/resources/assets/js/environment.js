e10d27384efd9167cab0628a64d36dceee537cb3
salamahin <danilasergeevich@gmail.com>
2015-11-19 00:52:43 +0300

    css start

--- src/main/resources/assets/js/environment.js
+++ src/main/resources/assets/js/environment.js
@@ -260,15 +260,18 @@ function resourceToListElem(resource) {
        });
 
     } else {
-       action = newLabel("Reserved by " + occupationInfo.userName + " on " + occupationInfo.occupationTime);
+       action = document.createTextNode("Reserved by " + occupationInfo.userName + " on " + occupationInfo.occupationTime);
     }
 
 
+
+    var label = newLabel(resource.name);
+    label.appendChild(action);
+    label.appendChild(btnAction);
+    label.appendChild(btnHistory);
+
     var listElem = newListElement();
-    listElem.appendChild(newLabel(resource.name));
-    listElem.appendChild(action);
-    listElem.appendChild(btnHistory);
-    listElem.appendChild(btnAction);
+    listElem.appendChild(label);
 
     return listElem;
 }
@@ -385,106 +388,4 @@ window.onload = function () {
     };
 
     Notification.requestPermission( newMessage );
-
-//  status = document.getElementById("status");
-//  environment = document.getElementById("environment");
-//  hist = document.getElementById("hist");
-//
-//  getStatus();
-//
-//  var websocket = new WebSocket(wsUri);
-//
-//  websocket.onopen = function(evt) {
-//    console.debug(evt.data);
-//  };
-//
-//  websocket.onclose = function(evt) {
-//    console.debug(evt.data);
-//  };
-//
-//  websocket.onmessage = function(evt) {
-//    console.debug(evt.data);
-//    status.innerHTML = evt.data;
-//    getStatus();
-//  };
-//
-//  websocket.onerror = function(evt) {
-//    console.error(evt.data)
-//  };
-//
-//
-//  var subscribe = document.getElementById("subscribe");
-//  var unsubscribe = document.getElementById("unsubscribe");
-//  var reserve = document.getElementById("reserve");
-//  var free = document.getElementById("free");
-//  var input = document.getElementById("input");
-//
-//  subscribe.onclick = function() {
-//    var xhr = new XMLHttpRequest();
-//    xhr.withCredentials = true;
-//    xhr.open("POST", "/teamnotifier/1.0/environment/server/subscribe/" + input.value, true);
-//    xhr.setRequestHeader("Authorization", "Basic " + btoa(user + ":" + pass));
-//    xhr.onreadystatechange = function() {reaskStatus(xhr);};
-//    xhr.send();
-//  }
-//
-//  unsubscribe.onclick = function() {
-//    var xhr = new XMLHttpRequest();
-//    xhr.withCredentials = true;
-//    xhr.open("DELETE", "/teamnotifier/1.0/environment/server/subscribe/" + input.value, true);
-//    xhr.setRequestHeader("Authorization", "Basic " + btoa(user + ":" + pass));
-//    xhr.onreadystatechange = function() {reaskStatus(xhr);};
-//    xhr.send();
-//  }
-//
-//  reserve.onclick = function() {
-//    var xhr = new XMLHttpRequest();
-//    xhr.withCredentials = true;
-//    xhr.open("POST", "/teamnotifier/1.0/environment/application/reserve/" + input.value, true);
-//    xhr.setRequestHeader("Authorization", "Basic " + btoa(user + ":" + pass));
-//    xhr.onreadystatechange = function() {reaskStatus(xhr);};
-//    xhr.send();
-//  }
-//
-//  free.onclick = function() {
-//    var xhr = new XMLHttpRequest();
-//    xhr.withCredentials = true;
-//    xhr.open("DELETE", "/teamnotifier/1.0/environment/application/reserve/" + input.value, true);
-//    xhr.setRequestHeader("Authorization", "Basic " + btoa(user + ":" + pass));
-//    xhr.onreadystatechange = function() {reaskStatus(xhr);};
-//    xhr.send();
-//  }
-//
-//
-//  var action_resource_id = document.getElementById("action_resource_id");
-//  var action_descr = document.getElementById("action_descr");
-//  var push_action = document.getElementById("push_action");
-//
-//  push_action.onclick = function() {
-//    var xhr = new XMLHttpRequest();
-//    xhr.withCredentials = true;
-//    xhr.open("POST", "/teamnotifier/1.0/environment/application/actions/" + action_resource_id.value, true);
-//    xhr.setRequestHeader("Authorization", "Basic " + btoa(user + ":" + pass));
-//    xhr.setRequestHeader("ActionDetails", action_descr.value)
-//    xhr.send();
-//  }
-//
-//  var action_hist_from = document.getElementById("action_hist_from");
-//  var action_hist_to = document.getElementById("action_hist_to");
-//  var get_actions = document.getElementById("get_actions");
-//
-//  get_actions.onclick = function() {
-//    var xhr = new XMLHttpRequest();
-//    xhr.withCredentials = true;
-//    xhr.open("GET", "/teamnotifier/1.0/environment/application/actions/" + action_resource_id.value, true);
-//    xhr.setRequestHeader("Authorization", "Basic " + btoa(user + ":" + pass));
-//    xhr.setRequestHeader("ActionsFrom", btoa(action_hist_from.value))
-//    xhr.setRequestHeader("ActionsTo", btoa(action_hist_to.value))
-//    xhr.onreadystatechange = function() {
-//      if (xhr.readyState == 4) {
-//        hist.innerHTML=xhr.responseText;
-//      }
-//    };
-//    xhr.send();
-//  }
 };
