1570b4fab1a078e23e1fa11bdc14c5bd4c09e970
salamahin <danilasergeevich@gmail.com>
2016-03-31 20:15:27 +0300

    interaction

--- src/main/assets2/js/showcase.js
+++ src/main/assets2/js/showcase.js
@@ -3,19 +3,41 @@ function openResources(resourcesNode) {
 		return;
 	
 	resourcesNode.classList.add("opened");
+	
+	var resourceSelectionbuttons = getAllResourceSelectionButtons();
+	for(var i = 0; i<resourceSelectionbuttons.length; i++)
+		deselectResource(getResourceNode(resourceSelectionbuttons[i]));
 }
 
 function closeResources(resourcesNode) {
 	resourcesNode.classList.remove("opened");
 }
 
-function getResourcesList(serverNode) {
-	return serverNode.parentNode.parentNode.childNodes[3];
+function selectResource(resourceNode) {
+	if(resourceNode.classList.contains("selected_resource"))
+		return;
+
+	resourceNode.classList.add("selected_resource");	
 }
 
+function deselectResource(resourceNode) {
+	resourceNode.classList.remove("selected_resource");
+}
 
-window.onload = function() {
-	var buttons = document.querySelectorAll(".server_selection_button");
+function getResourcesList(button) {
+	return button.parentNode.parentNode.childNodes[3];
+}
+
+function getResourceNode(button) {
+	return button.parentNode;
+}
+
+function getAllServerSelectionButtons() {
+	return document.querySelectorAll(".server_selection_button");
+}
+
+function installServerSelectionHandlers() {
+	var buttons = getAllServerSelectionButtons();
 
 	for(var i = 0; i<buttons.length; i++) {
 		const b=buttons[i];
@@ -29,4 +51,31 @@ window.onload = function() {
 			}
 		}
 	}
+}
+
+function getAllResourceSelectionButtons() {
+	return document.querySelectorAll(".resource_selection_button");
+}
+
+function installResourceSelectionHandlers() {
+	var buttons = getAllResourceSelectionButtons();
+
+	for(var i = 0; i<buttons.length; i++) {
+		const b=buttons[i];
+		b.onclick = function() {
+			for(var j = 0; j<buttons.length; j++) {
+                var resourceNode = getResourceNode(buttons[j]);
+				if(b == buttons[j])
+					selectResource(resourceNode);
+				else
+					deselectResource(resourceNode);
+			}
+		}
+	}
+}
+
+
+window.onload = function() {
+	installServerSelectionHandlers();
+	installResourceSelectionHandlers();
 }
