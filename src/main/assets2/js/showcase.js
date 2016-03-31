f1ad13ec1eb7bd515c9d48242002fc119d718e73
salamahin <danilasergeevich@gmail.com>
2016-03-31 19:44:26 +0300

    some animation

--- src/main/assets2/js/showcase.js
+++ src/main/assets2/js/showcase.js
@@ -1,27 +1,32 @@
-function toggleClass(elem, aClass) {
-	if(elem.classList.contains(aClass)) 
-		elem.classList.remove(aClass);
-	else
-		elem.classList.add(aClass);
+function openResources(resourcesNode) {
+	if(resourcesNode.classList.contains("opened"))
+		return;
+	
+	resourcesNode.classList.add("opened");
+}
+
+function closeResources(resourcesNode) {
+	resourcesNode.classList.remove("opened");
 }
 
 function getResourcesList(serverNode) {
 	return serverNode.parentNode.parentNode.childNodes[3];
 }
 
-function openResource(serverNode) {
-	var resources = getResourcesList(serverNode);
-	toggleClass(resources, "opened");
-}
 
 window.onload = function() {
 	var buttons = document.querySelectorAll(".server_selection_button");
 
-
 	for(var i = 0; i<buttons.length; i++) {
 		const b=buttons[i];
 		b.onclick = function() {
-			openResource(b);
+			for(var j = 0; j<buttons.length; j++) {
+                var resourcesNode = getResourcesList(buttons[j]);
+				if(b == buttons[j])
+					openResources(resourcesNode);
+				else
+					closeResources(resourcesNode);
+			}
 		}
 	}
 }
