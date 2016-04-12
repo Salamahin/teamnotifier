938c18f57c3cdd9b4c64df53b09316730732d46f
salamahin <danilasergeevich@gmail.com>
2016-04-12 23:46:20 +0300

    server/resource selection

--- src/main/web/js/sidemenuView.js
+++ src/main/web/js/sidemenuView.js
@@ -9,50 +9,52 @@ function SideMenulView() {
 		return server.subscribers != undefined && server.subscribers.includes(that.user);
 	}
 
-	function openResources(resourcesNode, server) {
-		if(resourcesNode.classList.contains("opened"))
-			return;
+	/*
+		Note, that "opened" class affects not the server_selection_button itself but .resources_list
+	*/
+	function selectServer(serverResourcesNode, server) {
+		if(!serverResourcesNode.classList.contains("opened"))
+			serverResourcesNode.classList.add("opened");
 		
-		resourcesNode.classList.add("opened");
-		
-		deselectOtherResources();
+		removeClassFromOthers(getAllResourcesLists(), serverResourcesNode, "opened");
+		removeClassFromOthers(getAllResourceSelectionButtonHolders(), undefined, "selected_resource");
+
 		that.serverSelectionHandler(server);
 	}
 
-	function closeResources(resourcesNode) {
-		resourcesNode.classList.remove("opened");
-	}
-
-	function selectResource(resourceNode, resource) {
-		if(resourceNode.classList.contains("selected_resource"))
+	/*
+		Note, that selected_resource class affects not the resource_selection_button itself, but the parent div-node
+	*/
+	function selectResource(resourceNodeHolder, resource) {
+		if(resourceNodeHolder.classList.contains("selected_resource"))
 			return;
 
-		resourceNode.classList.add("selected_resource");	
-		deselectOtherResources(resourceNode);
+		resourceNodeHolder.classList.add("selected_resource");	
+
+		removeClassFromOthers(getAllResourceSelectionButtonHolders(), resourceNodeHolder, "selected_resource");
 		that.resourceSelectionHandler(resource);
 	}
 
-	function deselectOtherResources(resourceNode) {
-		var resources = getAllResourceSelectionButtons();
-		for(var i = 0; i<resources.length; i++)
-			if(resources[i] != resourceNode)
-				resources[i].classList.remove("selected_resource");
+	function removeClassFromOthers(allNodes, node, aClass) {
+		for(var i = 0; i < allNodes.length; i++)
+			if(allNodes[i] != node)
+				allNodes[i].classList.remove(aClass);
 	}
 
-	function getResourcesList(button) {
-		return button.parentNode.parentNode.childNodes[3];
+	function getAllResourcesLists() {
+		return document.querySelectorAll(".resources_list");
 	}
 
-	function getResourceNode(button) {
-		return button.parentNode;
-	}
+	function getAllResourceSelectionButtonHolders() {
+		//since there is no parent selector in css we will do this manualy
 
-	function getAllServerSelectionButtons() {
-		return document.querySelectorAll(".server_selection_button");
-	}
+		var buttons = document.querySelectorAll(".resource_selection_button");
 
-	function getAllResourceSelectionButtons() {
-		return document.querySelectorAll(".resource_selection_button");
+		var holders = new Array(buttons.length);
+		for(var i = 0; i < buttons.length; i++)
+			holders[i] = buttons[i].parentNode;
+		
+		return holders;
 	}
 
 	function rebuildEnvironmentsView(environments) {
@@ -85,11 +87,13 @@ function SideMenulView() {
 
 	function findPresentListElemForEnvironmentAndServer(environment, server) {
 		var elems = document.querySelectorAll("#environments_list ul li");
+
 		for(var i = 0; i<elems.length; i++) {
 			var button = elems[i].querySelector("div div:nth-child(1)");
 			if(button.innerHTML== buildEnverironmentServerListItemName(environment, server))
 				return button;
 		}
+
 		return undefined;
 	}
 
@@ -101,7 +105,7 @@ function SideMenulView() {
 		selectionButton.classList.add("server_selection_button");
 		selectionButton.innerHTML= buildEnverironmentServerListItemName(environment, server);
 		selectionButton.onclick = function() {
-			openResources(resourcesList, server);
+			selectServer(resourcesList, server);
 		};
 
 		var innerDiv = document.createElement("div");
@@ -133,17 +137,18 @@ function SideMenulView() {
 	}
 
 	function createNewResource(resource) {
+		var innerDiv = document.createElement("div");
 		var resourceSelectionButton = document.createElement("a");
+
+		innerDiv.appendChild(resourceSelectionButton);
+
 		resourceSelectionButton.href = "#";
 		resourceSelectionButton.innerHTML= resource.name;
 		resourceSelectionButton.classList.add("resource_selection_button");
 		resourceSelectionButton.onclick = function() {
-			selectResource(resourceSelectionButton, resource);
+			selectResource(innerDiv, resource);
 		};
 		
-		var innerDiv = document.createElement("div");
-		innerDiv.appendChild(resourceSelectionButton);
-
 		var outerDiv = document.createElement("div");
 		outerDiv.appendChild(innerDiv);
 		if(resource.occupationInfo) {
